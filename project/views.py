8e09d7b7c8ccf073b59f3cc9940d2147d95d8a89
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-04-11 07:54:38 +0300

    Add files via upload

--- project/views.py
+++ project/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from main.models import projects
 
 # Create your views here.
+
+
+def homepage(request):
+    return render(request, template_name='main/home.html')
+
+
+def projectspage(request):
+    project = projects.objects.all()
+    return render(request, template_name='main/projects.html', context={'projects': project})
