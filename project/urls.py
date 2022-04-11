15b314f7a4c8ab7c2822d44968b80be17ebd5abc
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-04-11 12:22:52 +0300

    Add path to login form

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('home/', views.homepage, name='home'),
     path('projects/', views.projectspage, name='projects'),
-
+    path('login/', views.loginpage, name='login'),
+    path('logout/', views.logoutpage, name='logout'),
+    path('register/', views.registerpage, name='register'),
 ]
