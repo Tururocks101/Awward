2c850a27a18983985ae984eab1f015755d482846
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-04-11 07:51:19 +0300

    Add files via upload

--- project/models.py
+++ project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class projects(models.Model):
+
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1500)
+    image_url = models.CharField(max_length=520)
+
+    def __str__(self):
+        return self.name
