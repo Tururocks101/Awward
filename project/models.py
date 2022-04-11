ed460fd270bfa0f0b80425c66ad4e97578b60583
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-04-11 12:27:39 +0300

    Add the comments

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from post.models import Post
+# from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,3 +13,9 @@ class projects(models.Model):
 
     def __str__(self):
         return self.name
+
+#    class Comment(models.Model):
+        #  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+        #  user = models.ForeignKey(User, on_delete=models.CASCADE)
+        #  body = models.TextField()
+        #  date = models.DateTimeField(auto_now_add=True)
