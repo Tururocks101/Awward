5e1f402f097af9d6f6c9ebea1b439ca51e073a5c
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-04-11 19:48:31 +0300

    Add tests

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='tpoow', password='waord1234.')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
